2d6e8017977aeeac94acbc4a42fa382c7ab7f53b
m-akoth <marthyakoth1@gmail.com>
2023-02-23 05:13:04 -0800

    Multiple Mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * main - check the code if it returns positive or negative integer
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
