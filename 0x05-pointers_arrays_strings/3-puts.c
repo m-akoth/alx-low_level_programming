f69fc5cb4722c7f3e411c3cdcff4dd9573727345
m-akoth <marthyakoth1@gmail.com>
2023-02-27 05:49:17 -0800

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - checks the code
- *
- * Return: Always 0.
+ * _puts - Prints a string
+ * @str: String to be printed
+ * Return: Always 0
  */
-int main(void)
-{
-	    char *str;
 
-	        str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-		_puts(str);
-		return (0);
+void _puts(char *str)
+
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
 }
