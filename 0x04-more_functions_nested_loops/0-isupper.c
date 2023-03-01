dc6cf4dd022d16a39f6057437da17acf0ac06c95
m-akoth <marthyakoth1@gmail.com>
2023-03-01 01:56:43 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks if the letter is upper
+ * _isupper - Checks if the letter is upper
  * @c: Letter to be checked
  * Return: 1 if letter is upper or 0 if letter is lower
  */
