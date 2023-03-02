0f878332cab8b78442daae60b66b3ec461118aff
m-akoth <marthyakoth1@gmail.com>
2023-03-02 05:09:03 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,20 +2,23 @@
 
 /**
  * _strncat: Concatenates two strings
- * @dest: String to be appended upon
+ * @dest: String to be appended to by scr
  * @scr: String to be appended to dest
  * @n: Number of bytes from src to be appended to dest
  * Return: A pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
+
 {
 
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dest_len++;
+	dest_len++;
+
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	dest[dest_len++] = src[index];
+
 	return (dest);
 }
