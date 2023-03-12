3e17dfa7f938e625f14ba450a1b62b359e3714d4
m-akoth <marthyakoth1@gmail.com>
2023-03-12 11:52:03 -0700

    Create the library libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: Integer to be checked
+ * Return: Absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
